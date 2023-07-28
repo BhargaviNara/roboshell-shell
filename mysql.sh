a28e31cb34a914411b83c43312321e830166ce19
Bhargavi Nara <bhargavinara.informatica@gmail.com>
2023-07-29 07:28:52 +1200

    script

--- mysql.sh
+++ mysql.sh
@@ -4,9 +4,8 @@ yum module disable mysql -y
 
 yum install mysql-community-server -y
 
-systemctl enable mysqld
-systemctl start mysqld
-
 mysql_secure_installation --set-root-pass RoboShop@1
-
 mysql -uroot -pRoboShop@1
+
+systemctl enable mysqld
+systemctl restart mysqld
