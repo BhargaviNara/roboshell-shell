451383d2cd6f841ead8067316ba07601bea3202d
Bhargavi Nara <bhargavinara.informatica@gmail.com>
2023-07-29 19:10:55 +1200

    script

--- catalogue.sh
+++ catalogue.sh
@@ -1,15 +1,15 @@
-echo -e "\e[36m>>>>>>>>>create catalogue service<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>create catalogue service<<<<<<<<<<\e[0m" /tmp/roboshop.log
 cp catalogue.service /etc/systemd/system/catalogue.service
-echo -e "\e[36m>>>>>>>>>create mongo repo<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>create mongo repo<<<<<<<<<<\e[0m" /tmp/roboshop.log
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-echo -e "\e[36m>>>>>>>>>create catalogue service<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>create catalogue service<<<<<<<<<<\e[0m" /tmp/roboshop.log
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo -e "\e[36m>>>>>>>>install nodejs <<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>install nodejs <<<<<<<<<<\e[0m" /tmp/roboshop.log
 
 yum install nodejs -y
-echo -e "\e[36m>>>>>>>>>create app user<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>create app user<<<<<<<<<<\e[0m" /tmp/roboshop.log
 useradd roboshop
-echo -e "\e[36m>>>>>>>>>create app dir<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>create app dir<<<<<<<<<<\e[0m" /tmp/roboshop.log
 mkdir /app
 echo -e "\e[36m>>>>>>>>>download app content<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
